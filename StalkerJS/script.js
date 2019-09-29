658e52129c08bba76a86c1ec74798ad7c5cf7962
xyfka <lukasz.maz@gmail.com>
2019-09-29 22:14:06 +0200

    First stage

---
+++ StalkerJS/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Your first name:");
+var lastName = prompt("Your last name:");
+var age = prompt("Your age:");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
